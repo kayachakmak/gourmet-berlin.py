52b1d5553951430493cc16b60a77ab83ef9128fe
Kaya <kycakmak@gmail.com>
2024-06-13 22:59:47 +0200

    add redis addon to heroku

--- config.py
+++ config.py
@@ -12,7 +12,6 @@ app=Flask(__name__)
 CORS(app, supports_credentials=True)
 
 db_path = os.path.join(os.path.dirname(__file__), 'mydatabase.db')
-print(f"Database will be created at: {db_path}")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
@@ -21,7 +20,8 @@ app.config["SECRET_KEY"]=os.environ["SECRET_KEY"]
 app.config["SESSION_TYPE"]= "redis"
 app.config["SESSION_PERMANENT"]= False
 app.config["SESSION_USE_SIGNER"]= True
-app.config["SESSION_REDIS"]= redis.from_url("redis://127.0.0.1:6379")
 
+redis_url = os.getenv("REDIS_URL", "redis://127.0.0.1:6379")
+app.config["SESSION_REDIS"] = redis.from_url(redis_url)
 
 db=SQLAlchemy(app)
